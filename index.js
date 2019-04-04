497477d8c147624036a744f9d695ac399ec2639f
Cody Lawdermilt <lawdermiltcody@gmail.com>
2019-04-04 04:51:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log('HELLO'.toUpperCase()) ;
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase()) ;
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = "I can't hear you!";
+  return lowercase;
+}
+
+
+function sayHiToGrandma(string) {
+  var uppercase = "YES INDEED!";
+  return uppercase;
+}
+
